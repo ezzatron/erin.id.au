fa57840d99e5a3f1dbfe64a62e3f593e0189d77e
Erin Millard <contact@erin.id.au>
2025-07-03 19:03:31 +1000

    Add vCard

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,18 +3,13 @@ import { Container } from "@/components/Container";
 import { BlueskyIcon } from "@/components/icon/BlueskyIcon";
 import { GitHubIcon } from "@/components/icon/GitHubIcon";
 import { LinkedInIcon } from "@/components/icon/LinkedInIcon";
-import logoAirbnb from "@/images/logos/airbnb.svg";
-import logoFacebook from "@/images/logos/facebook.svg";
-import logoPlanetaria from "@/images/logos/planetaria.svg";
-import logoStarbucks from "@/images/logos/starbucks.svg";
 import portraitImage from "@/images/portrait.jpg";
 import clsx from "clsx";
 import { ArrowDownIcon, MailIcon } from "lucide-react";
 import { type Metadata } from "next";
-import Image, { type ImageProps } from "next/image";
+import Image from "next/image";
 import Link from "next/link";
 import type { ComponentType, ReactNode } from "react";
-import { BriefcaseIcon } from "../../components/icon/BriefcaseIcon";
 
 export const metadata: Metadata = {
   title: "About",
@@ -71,8 +66,37 @@ export default function AboutPage() {
           </div>
         </div>
         <div className="flex flex-col gap-16 lg:pl-20">
+          <div className="flex flex-col gap-4">
+            <Button href="#TODO" variant="primary" className="group w-full">
+              Download CV
+              <ArrowDownIcon
+                strokeWidth={2}
+                className="size-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50"
+              />
+            </Button>
+            <Button
+              href="/Erin%20Millard-Wright.vcf"
+              variant="secondary"
+              className="group w-full"
+            >
+              Download contact
+              <ArrowDownIcon
+                strokeWidth={2}
+                className="size-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50"
+              />
+            </Button>
+          </div>
+
           <ul role="list">
-            <SocialLink href="#" icon={GitHubIcon}>
+            <SocialLink href="mailto:spencer@planetaria.tech" icon={MailIcon}>
+              spencer@planetaria.tech
+            </SocialLink>
+
+            <SocialLink
+              href="#"
+              icon={GitHubIcon}
+              className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
+            >
               Follow on GitHub
             </SocialLink>
             <SocialLink href="#" icon={LinkedInIcon} className="mt-4">
@@ -81,16 +105,7 @@ export default function AboutPage() {
             <SocialLink href="#" icon={BlueskyIcon} className="mt-4">
               Follow on Bluesky
             </SocialLink>
-            <SocialLink
-              href="mailto:spencer@planetaria.tech"
-              icon={MailIcon}
-              className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
-            >
-              spencer@planetaria.tech
-            </SocialLink>
           </ul>
-
-          <Resume />
         </div>
       </div>
     </Container>
@@ -118,107 +133,5 @@ function SocialLink({
         <span className="ml-4">{children}</span>
       </Link>
     </li>
-  );
-}
-
-interface Role {
-  company: string;
-  title: string;
-  logo: ImageProps["src"];
-  start: string | { label: string; dateTime: string };
-  end: string | { label: string; dateTime: string };
-}
-
-function Role({ role }: { role: Role }) {
-  const startLabel =
-    typeof role.start === "string" ? role.start : role.start.label;
-  const startDate =
-    typeof role.start === "string" ? role.start : role.start.dateTime;
-
-  const endLabel = typeof role.end === "string" ? role.end : role.end.label;
-  const endDate = typeof role.end === "string" ? role.end : role.end.dateTime;
-
-  return (
-    <li className="flex gap-4">
-      <div className="relative mt-1 flex size-10 flex-none items-center justify-center rounded-full shadow-md ring-1 shadow-zinc-800/5 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-        <Image src={role.logo} alt="" className="size-7" unoptimized />
-      </div>
-      <dl className="flex flex-auto flex-wrap gap-x-2">
-        <dt className="sr-only">Company</dt>
-        <dd className="w-full flex-none text-sm font-medium text-zinc-900 dark:text-zinc-100">
-          {role.company}
-        </dd>
-        <dt className="sr-only">Role</dt>
-        <dd className="text-xs text-zinc-500 dark:text-zinc-400">
-          {role.title}
-        </dd>
-        <dt className="sr-only">Date</dt>
-        <dd
-          className="ml-auto text-xs text-zinc-400 dark:text-zinc-500"
-          aria-label={`${startLabel} until ${endLabel}`}
-        >
-          <time dateTime={startDate}>{startLabel}</time>{" "}
-          <span aria-hidden="true">—</span>{" "}
-          <time dateTime={endDate}>{endLabel}</time>
-        </dd>
-      </dl>
-    </li>
-  );
-}
-
-function Resume() {
-  const resume: Array<Role> = [
-    {
-      company: "Planetaria",
-      title: "CEO",
-      logo: logoPlanetaria,
-      start: "2019",
-      end: {
-        label: "Present",
-        dateTime: new Date().getFullYear().toString(),
-      },
-    },
-    {
-      company: "Airbnb",
-      title: "Product Designer",
-      logo: logoAirbnb,
-      start: "2014",
-      end: "2019",
-    },
-    {
-      company: "Facebook",
-      title: "iOS Software Engineer",
-      logo: logoFacebook,
-      start: "2011",
-      end: "2014",
-    },
-    {
-      company: "Starbucks",
-      title: "Shift Supervisor",
-      logo: logoStarbucks,
-      start: "2008",
-      end: "2011",
-    },
-  ];
-
-  return (
-    <div className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
-      <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
-        <BriefcaseIcon className="size-6 flex-none fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500" />
-        <span className="ml-3">Work</span>
-      </h2>
-      <ol className="mt-6 space-y-4">
-        {resume.map((role, roleIndex) => (
-          <Role key={roleIndex} role={role} />
-        ))}
-      </ol>
-      <Button href="#" variant="secondary" className="group mt-6 w-full">
-        Download CV
-        <ArrowDownIcon
-          strokeWidth={2}
-          className="size-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50"
-        />
-      </Button>
-    </div>
   );
 }
