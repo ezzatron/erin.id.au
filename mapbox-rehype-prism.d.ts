35c590ad39060715bd391df16095198223027f45
Erin Millard <contact@erin.id.au>
2025-07-03 12:05:29 +1000

    Remove MDX and prism

--- mapbox-rehype-prism.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "@mapbox/rehype-prism" {
-  const rehypePrism: () => void;
-
-  export default rehypePrism;
-}
--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,11 @@
-import rehypePrism from "@mapbox/rehype-prism";
 import bundleAnalyzer from "@next/bundle-analyzer";
-import nextMDX from "@next/mdx";
 import type { NextConfig } from "next";
-import remarkGfm from "remark-gfm";
 
-const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypePrism],
-  },
+export default createConfig({
+  distDir: "artifacts/next/dist",
+  output: "standalone",
+  reactStrictMode: true,
 });
-
-export default withMDX(
-  createConfig({
-    distDir: "artifacts/next/dist",
-    output: "standalone",
-    pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
-    reactStrictMode: true,
-  }),
-);
 
 function createConfig(config: NextConfig): NextConfig {
   return process.env.ANALYZE === "true"
