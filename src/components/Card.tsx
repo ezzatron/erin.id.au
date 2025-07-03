a7984f6561e3a725c19f7b33fa66d88756c862f6
Erin Millard <contact@erin.id.au>
2025-07-03 14:06:24 +1000

    Remove dead code

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,5 @@
 import clsx from "clsx";
 import Link from "next/link";
-import { ChevronRightIcon } from "./icon/ChevronRightIcon";
 
 export function Card<T extends React.ElementType = "div">({
   as,
@@ -36,23 +35,6 @@ Card.Link = function CardLink({
   );
 };
 
-Card.Title = function CardTitle<T extends React.ElementType = "h2">({
-  as,
-  href,
-  children,
-}: Omit<React.ComponentPropsWithoutRef<T>, "as" | "href"> & {
-  as?: T;
-  href?: string;
-}) {
-  const Component = as ?? "h2";
-
-  return (
-    <Component className="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
-      {href ? <Card.Link href={href}>{children}</Card.Link> : children}
-    </Component>
-  );
-};
-
 Card.Description = function CardDescription({
   children,
 }: {
@@ -62,51 +44,5 @@ Card.Description = function CardDescription({
     <p className="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
       {children}
     </p>
-  );
-};
-
-Card.Cta = function CardCta({ children }: { children: React.ReactNode }) {
-  return (
-    <div
-      aria-hidden="true"
-      className="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500"
-    >
-      {children}
-      <ChevronRightIcon className="ml-1 h-4 w-4 stroke-current" />
-    </div>
-  );
-};
-
-Card.Eyebrow = function CardEyebrow<T extends React.ElementType = "p">({
-  as,
-  decorate = false,
-  className,
-  children,
-  ...props
-}: Omit<React.ComponentPropsWithoutRef<T>, "as" | "decorate"> & {
-  as?: T;
-  decorate?: boolean;
-}) {
-  const Component = as ?? "p";
-
-  return (
-    <Component
-      className={clsx(
-        className,
-        "relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 dark:text-zinc-500",
-        decorate && "pl-3.5",
-      )}
-      {...props}
-    >
-      {decorate && (
-        <span
-          className="absolute inset-y-0 left-0 flex items-center"
-          aria-hidden="true"
-        >
-          <span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500" />
-        </span>
-      )}
-      {children}
-    </Component>
   );
 };
